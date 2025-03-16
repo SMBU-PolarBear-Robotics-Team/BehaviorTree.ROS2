0fb45a3c62b2f8b17a6f50d530ff5e1a0b9c72b7
LihanChen2004 <757003373@qq.com>
2025-03-16 21:52:45 +0800

    Revert "🎈 perf(bt_topic_pub_action_node): always publish in RUNNING status"
    This reverts commit 06f3965fad8a59d24f77f0cdf09a1bfb6d8894e8.

--- behaviortree_ros2/include/behaviortree_ros2/bt_topic_pub_action_node.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/bt_topic_pub_action_node.hpp
@@ -121,7 +121,6 @@ private:
   std::shared_ptr<Publisher> publisher_;
   msec duration_;
   rclcpp::Time start_time_;
-  TopicT publish_msg_;
 
   bool createPublisher(const std::string& topic_name);
 };
@@ -218,8 +217,7 @@ inline NodeStatus RosTopicPubStatefulActionNode<T>::onStart()
   {
     return NodeStatus::FAILURE;
   }
-  publish_msg_ = msg;
-  publisher_->publish(publish_msg_);
+  publisher_->publish(msg);
 
   // Record start time
   start_time_ = node_->now();
@@ -231,7 +229,6 @@ inline NodeStatus RosTopicPubStatefulActionNode<T>::onRunning()
 {
   auto elapsed = node_->now() - start_time_;
   auto elapsed_ms = elapsed.to_chrono<msec>();
-  publisher_->publish(publish_msg_);
 
   return (elapsed_ms < duration_) ? NodeStatus::RUNNING : NodeStatus::SUCCESS;
 }
